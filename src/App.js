c67efd1dd3caa006b3e7e3928719e784110b44b2
Abbel Dawit <abbeldawit@abbels-air.mynetworksettings.com>
2025-02-21 16:10:59 -0500

    Initial commit - Ebook Store Frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Ebook Store</h1>
+      <p>Your one-stop shop for ebooks.</p>
+    </div>
+  );
+}
+
+export default App;
